65817f1af8f2b03bae24f0bcde39da821bf35100
Nafeez Ahamed Mohamed <nmoham46@asu.edu>
2025-11-07 22:37:45 -0700

    feat : added UserStoryForm in the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import '@/styles/App.css'
+import UserStoryForm from './components/UserStoryForm'
 
 function App() {
   return (
     <div className="">
       Sleek Board
+      <UserStoryForm />
     </div>
   )
 }
