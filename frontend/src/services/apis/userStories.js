bded1aba9a10efdb79f79526c0c6e931bb72d28c
gdusane-asu <gduasane@asu.edu>
2025-11-07 23:57:24 -0700

    feat: corrected url for backend

--- frontend/src/services/apis/userStories.js
+++ frontend/src/services/apis/userStories.js
@@ -1,12 +1,12 @@
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export async function createUserStory(data) {
   try {
-    const response = await fetch('http://localhost:5050/api/userstories', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    const response = await fetch(`${BASE_URL}/api/v1/user-stories`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
-
-    return response.json();
+    return await response.json();
   } catch (err) {
     console.error("Error creating user story:", err);
     throw err;
