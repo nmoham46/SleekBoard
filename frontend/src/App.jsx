8588f8f1a2fde7ec42a2f516c7cdf82dc58aa2c3
Karim Omar <kmomar1@asu.edu>
2025-11-09 12:57:20 -0700

    refactor: (Frontend) navbar folder and import alias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import '@/styles/App.css'
-import { Navigationbar } from './components/Navbar'
+import { Navigationbar } from '@/components/layout/navbar/Navbar'
 
 function App() {
   return (
     <div className="p-4">
-
       <Navigationbar />
     </div>
   )
