4c82899d16af57146e55f039e16db79f9ad0b57e
Nafeez Ahamed Mohamed <nmoham46@asu.edu>
2025-11-20 13:20:48 -0700

    feat : Handled response in a better way

--- frontend/src/services/apis/comments.js
+++ frontend/src/services/apis/comments.js
@@ -1,57 +1,59 @@
-import Http from "@/plugins/Http"
+// src/services/apis/comments.js
+import Http from "@/plugins/Http";
+import { HttpStatusCode } from "axios";
 
 export const addComment = async (data) => {
-  const path = `/api/v1/comments/`
+  const path = `/api/v1/comments/`;
 
   const options = {
     method: "POST",
     url: path,
-    data: data
-  }
+    data,
+  };
 
   try {
-    await Http(options)
+    const res = await Http(options);
+    return res?.data ?? res; 
+  } catch (error) {
+    console.error("Failure in addComments api call");
+    console.error("Error: " + error);
+    throw new Error();
   }
-  catch (error) {
-    console.error("Failure in addComments api call")
-    console.error("Error: " + error)
-    throw new Error()
-  }
-}
+};
 
 export const updateComment = async (id, data) => {
-  const path = `/api/v1/comments/${id}`
+  const path = `/api/v1/comments/${id}`;
 
   const options = {
     method: "PUT",
     url: path,
-    data: data
-  }
+    data,
+  };
 
   try {
-    await Http(options)
+    const res = await Http(options);
+    return res?.data ?? res; 
+  } catch (error) {
+    console.error("Failure in updateComment api call");
+    console.error("Error: " + error);
+    throw new Error();
   }
-  catch (error) {
-    console.error("Failure in updateComment api call")
-    console.error("Error: " + error)
-    throw new Error()
-  }
-}
+};
 
 export const deleteComment = async (id) => {
-  const path = `/api/v1/comments/${id}`
+  const path = `/api/v1/comments/${id}`;
 
   const options = {
     method: "DELETE",
-    url: path
-  }
+    url: path,
+  };
 
   try {
-    await Http(options)
+    const res = await Http(options);
+    return res?.data ?? res;
+  } catch (error) {
+    console.error("Failure in Delete Comment api call");
+    console.error("Error: " + error);
+    throw new Error();
   }
-  catch (error) {
-    console.error("Failure in Delete Comment api call")
-    console.error("Error: " + error)
-    throw new Error()
-  }
-}
+};
