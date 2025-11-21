2cfd785d85a1150730bbc6ef89e84427891993b4
Karim Omar <kmomar1@asu.edu>
2025-11-21 14:34:13 -0700

    feat: (frontend) Create and configure system wide global loader

---
+++ frontend/src/components/layout/loaders/GlobalLoader.jsx
@@ -0,0 +1,18 @@
+import ClassicLoader from "@/components/common/loaders/ClassicLoader"
+import { useLoader } from "@/context/LoaderContext"
+
+const GlobalLoader = () => {
+  const { isGlobalLoading } = useLoader()
+
+  return (
+    <div>
+      {isGlobalLoading && (
+        <div className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-80 flex justify-center items-center z-[9999]">
+          <ClassicLoader />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default GlobalLoader
